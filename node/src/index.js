aef2232e356a9f31ed56e049e2d9aeee25600e1b
lesteven <le_steven@outlook.com>
2019-06-18 10:40:11 -0700

    feat: add node server

---
+++ node/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const debug = require('debug')('http');
+
+const app = express();
+
+app.get('/', (req,res) => {
+  res.send('Hello World!\n');
+})
+
+if (app.get('env') === 'development') {
+  debug('Development mode!');
+} else {
+  debug('Production mode!');
+}
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  debug(`Listening on port ${port}`);
+});
